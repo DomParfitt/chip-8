7e88dab3f80a6fc7517556dfa04296e9cae5a463
Parfitt <dom.parfitt@cgi.com>
2018-05-30 11:54:30 +0100

    Display fixes and variable name changes

--- src/main.rs
+++ src/main.rs
@@ -13,25 +13,24 @@ fn main() {
     let mut chip8 = chip8::Chip8::new();
     chip8.memory[0x200] = 0xA2;
     chip8.memory[0x201] = 0xF0;
-    println!("Value at I: {:X}", chip8.I);
+    println!("Value at I: {:X}", chip8.i);
     chip8.emulate_cycle();
-    println!("Value at I: {:X}", chip8.I);
+    println!("Value at I: {:X}", chip8.i);
 
     chip8.graphics[224] = 1;
     chip8.print_display();
 
     while let Some(e) = window.next() {
         window.draw_2d(&e, |context, graphics| {
-            clear([0.0, 0.0, 0.0, 1.0], graphics);
+            clear([0.5, 0.5, 0.5, 1.0], graphics);
 
             for y in 0..32 {
                 for x in 0..64 {
                     let mut color = [0.0, 0.0, 0.0, 1.0];
                     if chip8.graphics[x + y * 64] != 0 {
-                        println!("Pixel should be white [{}]", x+y*64);
                         color = [1.0, 1.0, 1.0, 1.0];
                     } 
-                    rectangle(color, [x as f64, y as f64, 10.0, 10.0], context.transform, graphics);
+                    rectangle(color, [10.0 * x as f64, 10.0 * y as f64, 10.0, 10.0], context.transform, graphics);
                 }
             }
 
