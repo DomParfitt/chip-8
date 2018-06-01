a51ba0ca391fa93122595079c3a7835a855669a8
Dom Parfitt <dom.parfitt@gmail.com>
2018-06-01 13:33:15 +0100

    Began adding keyboard handling

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,51 @@
+extern crate piston;
 extern crate piston_window;
 extern crate rand;
 
 use piston_window::{clear, rectangle, PistonWindow, WindowSettings};
+use piston::input::*;
+use piston::event_loop::*;
 
+mod assembler;
 mod chip8;
 mod sprite;
-mod assembler;
 
 fn main() {
     let mut window: PistonWindow = WindowSettings::new("CHIP 8", [64 * 10, 32 * 10])
         .build()
         .unwrap();
     let mut chip8 = chip8::Chip8::new();
+    chip8.load("C:\\Users\\parfittdr\\projects\\chip-8\\roms\\pong".to_string());
 
-    for i in 0..12 {
-        //Set origin
-        chip8.memory[0x200 + i * 8] = 0x60;
-        chip8.memory[0x201 + i * 8] = 0x32 + (5 * i as u8) + 1;
-        chip8.memory[0x202 + i * 8] = 0x61;
-        chip8.memory[0x203 + i * 8] = 0x01;
+    // for i in 0..12 {
+    //     //Set origin
+    //     chip8.memory[0x200 + i * 8] = 0x60;
+    //     chip8.memory[0x201 + i * 8] = 0x32 + (5 * i as u8) + 1;
+    //     chip8.memory[0x202 + i * 8] = 0x61;
+    //     chip8.memory[0x203 + i * 8] = 0x01;
 
-        //Load 0x000 into I
-        chip8.memory[0x204 + i * 8] = 0xA0;
-        chip8.memory[0x205 + i * 8] = 0x00 + 5 * i as u8;
+    //     //Load 0x000 into I
+    //     chip8.memory[0x204 + i * 8] = 0xA0;
+    //     chip8.memory[0x205 + i * 8] = 0x00 + 5 * i as u8;
 
-        //Draw
-        chip8.memory[0x206 + i * 8] = 0xD0;
-        chip8.memory[0x207 + i * 8] = 0x15;
-    }
+    //     //Draw
+    //     chip8.memory[0x206 + i * 8] = 0xD0;
+    //     chip8.memory[0x207 + i * 8] = 0x15;
+    // }
 
     // chip8.graphics[224] = true;
     // chip8.print_display();
 
     while let Some(e) = window.next() {
         chip8.emulate_cycle();
+        
+        if let Some(key_pressed) = e.press_args() {
+        }
+
+        if let Some(key_released) = e.release_args() {
+
+        }
+
         if chip8.V[0xF] != 0 {
             window.draw_2d(&e, |context, graphics| {
                 clear([0.5, 0.5, 0.5, 1.0], graphics);
