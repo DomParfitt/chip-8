bc99ad2e4367fc90d5bbf8cf06bb7af1f23ea9f1
Dom Parfitt <dom.parfitt@gmail.com>
2018-06-18 12:58:58 +0100

    Added some unit tests

--- src/chip8/tests.rs
+++ src/chip8/tests.rs
@@ -1,8 +1,25 @@
 use chip8::Chip8;
 
+
+#[test]
+fn test_call() {
+    let mut cpu = init_cpu_with_program(vec!(0x22, 0x22));
+    cpu.emulate_cycle();
+    assert_eq!(cpu.pc, 0x222);
+}
+
 #[test]
 fn test_call_and_return() {
-    let mut cpu = Chip8::new();
-    cpu.load_program(vec!(0x22, 0x22, 0x00, 0xEE));
+    let mut cpu = init_cpu_with_program(vec!(0x22, 0x04, 0x00, 0x00, 0x00, 0xEE));
+    cpu.emulate_cycle();
+    cpu.emulate_cycle();
+    println!("Stack: {:X?}", cpu.stack);
+    println!("PC: {:X}", cpu.pc);
+    assert_eq!(cpu.pc, 0x202);
+}
 
+fn init_cpu_with_program(program: Vec<u8>) -> Chip8 {
+    let mut cpu = Chip8::new();
+    cpu.load_program(program);
+    cpu
 }
